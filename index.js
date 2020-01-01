876fa4ec4fc4973e68cb4df0a19610e4363d41ba
NBM <notbigmuzzy@gmail.com>
2020-01-01 19:58:54 +0100

    v4 - default layout/main page

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 $(document).ready(function() {
+    //INDEX
+    shellToggle()
 
-    console.log('1')
+    //FUNCTIONS
+    function shellToggle() {
+        if (document.getElementById('js-the-shell')) {
+            document.addEventListener('keydown', (e) => {
+                if (e.code == "F1") {
+                    e.preventDefault()
+                }
+            });
+            document.addEventListener('keyup', (e) => {
+                if (e.code == "F1") {
+                    e.preventDefault()
 
-
+                    $('body').toggleClass('show-shell')
+                }
+            });
+            $('#f1').click(function() {
+                $('body').toggleClass('show-shell')
+            })
+        }
+    }
 })
