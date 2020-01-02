407ae056718bb14678c32f6da9915f9e7734f908
NBM <notbigmuzzy@gmail.com>
2020-01-02 17:59:15 +0100

    v4 - more shell stylings

--- index.js
+++ index.js
@@ -1,25 +1,51 @@
-$(document).ready(function() {
+$(document).ready(function () {
     //INDEX
     shellToggle()
+    urlEventListeners()
+    window.onhashchange = urlEventListeners;
 
     //FUNCTIONS
     function shellToggle() {
-        if (document.getElementById('js-the-shell')) {
-            document.addEventListener('keydown', (e) => {
-                if (e.code == "F1") {
-                    e.preventDefault()
-                }
-            });
-            document.addEventListener('keyup', (e) => {
-                if (e.code == "F1") {
-                    e.preventDefault()
+        document.addEventListener('keydown', (e) => {
+            if (e.code == "F1") {
+                e.preventDefault()
+                $('#f1').addClass('highlight');
+            }
+        });
+        document.addEventListener('keyup', (e) => {
+            if (e.code == "F1") {
+                e.preventDefault()
+                window.location.hash = '#show-shell';
+                $('#f1').removeClass('highlight');
+            }
+        });
+        $('.shell-off').click(function (e) {
+            e.preventDefault();
+            window.history.back();
+        })
+    }
 
-                    $('body').toggleClass('show-shell')
-                }
-            });
-            $('#f1').click(function() {
-                $('body').toggleClass('show-shell')
-            })
+    function urlEventListeners() {
+        if (document.getElementById('portfolio')) {
+            var e = location.hash;
+
+            switch (e) {
+                case "#work":
+                    $('body').removeClass().addClass('show-work')
+                    break;
+                case "#me":
+                    $('body').removeClass().addClass('show-me')
+                    break;
+                case "#blog":
+                    $('body').removeClass().addClass('show-blog')
+                    break;
+                case "#show-shell":
+                    $('body').addClass('show-shell')
+                    break;
+                default:
+                    $('body').removeClass()
+                    break;
+            }
         }
     }
 })
