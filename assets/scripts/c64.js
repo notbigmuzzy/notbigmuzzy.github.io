08910cb6dfc0aff5ba0e5f3a6ea53a8e638f6cdc
NBM <notbigmuzzy@gmail.com>
2020-01-04 19:59:22 +0100

    v4 - c64 load comm

--- assets/scripts/c64.js
+++ assets/scripts/c64.js
@@ -3,41 +3,23 @@ $(document).ready(function () {
     inputFocus()
 
     //INTERACTIVE - ALWAYS FOCUS LATEST INPUT, PREVENT TAB
+    document.onkeydown = keydown;
+    function keydown(e) {
+        if (!e) e = event;
+        if (e.ctrlKey && e.keyCode == 67) {
+            clearPrompt();
+        }
+        if (e.keyCode == 9) {
+            e.preventDefault()
+        }
+    }
     $(document).on('click', 'body', function () {
         inputFocus()
     })
-    document.addEventListener('keydown', (e) => {
-        if (e.code == "Tab") {
-            e.preventDefault()
-        }
-    });
     $(document).on('submit', '.editor-input .commandbox.active form', function (e) {
         e.preventDefault();
         theBrain($(this));
     })
-
-    //FUNCTIONS
-    function inputFocus() {
-        $('.editor-input .commandbox.active input').focus();
-    }
-    function help() {
-        var c64Help = $('.c64-helper').html()
-        $('.editor-input .commandbox.active').removeClass('active').after(c64Help)
-        inputFocus()
-    }
-    function restartPromptAfterError(elem) {
-        $('.editor-input .commandbox.active').removeClass('active').after('<div class="commandbox active"><span class="title">?SYNTAX ERROR.</span><span class="title">?<i class="no-command">' + elem + '</i> Command not found.</span><form autocomplete="off"><input type="text" id="editor" name="editor" autofocus="true"/></form></div>')
-        inputFocus()
-    }
-    function restartPromptAfterSuccess(elem) {
-        $('.commandbox.active').removeClass('active').after('<div class="commandbox active"><span class="title">SEARCHING FOR ' + elem + '</span><span class="title">Loading</span><span class="title">Ready.</span><form autocomplete="off"><input type="text" id="editor" name="editor" autofocus="true"/></form></div>')
-        inputFocus()
-    }
-    function clearPrompt() {
-        $('.editor-input .commandbox').remove()
-        $('.editor-input').append('<div class="commandbox active"><span class="title">Ready.</span><form autocomplete="off"><input type="text" id="editor" name="editor" autofocus="true"/></form></div>')
-        inputFocus()
-    }
 
     //MAIN LOGIC
     function theBrain(submitedForm) {
@@ -51,7 +33,7 @@ $(document).ready(function () {
             var $submitCondition = $submitValue;
         }
 
-        switch ($submitCondition) {
+        switch ($submitCondition.toLowerCase()) {
             case "help":
                 help()
                 break;
@@ -66,26 +48,78 @@ $(document).ready(function () {
                 restartPromptAfterSuccess('shell')
                 break;
             case "list":
-                console.log('LIST')
+                if ($submitParam) {
+                    console.log($submitParam)
+
+                    if ($submitParam == 'blog') {
+                        getShit('creative/blogposts')
+                    } else if ($submitParam == 'work') {
+                        getShit('creative/worklists')
+                    } else {
+                        getShit($submitParam)
+                    }
+
+                } else {
+                    restartPromptNoLoadParams($submitCondition)
+                }
                 break;
             case "load":
-                break;
-            case "get":
                 if ($submitParam) {
-                    $.ajax({
-                        url: $submitParam,
-                        success: function (returnData) {
-                            console.log(data);
-                        }
-                    });
-                    restartPromptAfterSuccess($submitParam)
+                    getShit($submitParam)
                 } else {
-                    restartPromptAfterError($submitCondition)
+                    restartPromptNoLoadParams($submitCondition)
                 }
                 break;
             default:
                 restartPromptAfterError($submitCondition)
                 break;
         }
+    }
+
+    //FUNCTIONS
+    function inputFocus() {
+        $('.editor-input .commandbox.active input').focus();
+    }
+    function help() {
+        var c64Help = $('.c64-helper').html()
+        $('.editor-input .commandbox.active').removeClass('active').after(c64Help)
+        inputFocus()
+    }
+    function restartPromptAfterError(elem) {
+        $('.editor-input .commandbox.active').removeClass('active').after('<div class="commandbox active"><span class="title">?SYNTAX ERROR.</span><span class="title">?<i class="no-command">' + elem + '</i> Command not found.</span><span class="title">Ready.</span><form autocomplete="off"><input type="text" id="editor" name="editor" autofocus="true"/></form></div>')
+        inputFocus()
+    }
+    function restartPromptAfterSuccess(elem) {
+        $('.editor-input .commandbox.active').removeClass('active').after('<div class="commandbox active"><span class="title">SEARCHING FOR ' + elem + '</span><span class="title">Loading</span><span class="title">Ready.</span><form autocomplete="off"><input type="text" id="editor" name="editor" autofocus="true"/></form></div>')
+        inputFocus()
+    }
+    function restartPromptNoLoadParams(elem) {
+        $('.editor-input .commandbox.active').removeClass('active').after('<div class="commandbox active"><span class="title">?SYNTAX ERROR.</span><span class="title">?<i class="no-command">' + elem + '</i> has no valid parameters.</span><span class="title">Type <i class="no-command">HELP</i>.</span><span class="title">ready.</span><form autocomplete="off"><input type="text" id="editor" name="editor" autofocus="true"/></form></div>')
+        inputFocus()
+    }
+    function restartPromptLoadError(elem) {
+        $('.editor-input .commandbox.active').removeClass('active').after('<div class="commandbox active"><span class="title">?LOAD ERROR.</span><span class="title">?<i class="no-command">' + elem + '</i> NOT FOUND.</span></span><span class="title">ready.</span><form autocomplete="off"><input type="text" id="editor" name="editor" autofocus="true"/></form></div>')
+        inputFocus()
+    }
+    function clearPrompt() {
+        $('.editor-input .commandbox').remove()
+        $('.editor-input').append('<div class="commandbox active"><span class="title">Ready.</span><form autocomplete="off"><input type="text" id="editor" name="editor" autofocus="true"/></form></div>')
+        inputFocus()
+    }
+    function writeOut(elem) {
+        $('.editor-input .commandbox.active').before('<div class="commandbox writeout-text">' + elem + '</div>');
+    }
+    function getShit(elem) {
+        $.ajax({
+            url: elem,
+            dataType: "html",
+            success: function (response) {
+                restartPromptAfterSuccess('content')
+                writeOut(response)
+            },
+            error: function (response) {
+                restartPromptLoadError(response.status)
+            }
+        });
     }
 })
