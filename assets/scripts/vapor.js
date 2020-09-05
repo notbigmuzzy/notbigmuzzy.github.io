44d3359571544f91b7f4da25832aefd4fff2e398
nbm <notbigmuzzy@gmail.com>
2020-09-05 04:20:54 +0200

    Update shell logic for vapor page

--- assets/scripts/vapor.js
+++ assets/scripts/vapor.js
@@ -1,5 +1,16 @@
 $(document).ready(function () {
 
+	//SET STUFF TO BE DRAGGABLE
 	$( ".icon" ).draggable();
 
+	//SET HASH ON VARIOUS CLICK
+	$(".action").click(function() {
+		var $action = $(this).attr('data-action')
+		window.location.hash = $action;
+	})
+
+	//CLEAR HASH ON DESKTOP CLICK
+	$(".clear-all-actions").click(function() {
+		history.pushState("", document.title, window.location.pathname);
+	})
 })
