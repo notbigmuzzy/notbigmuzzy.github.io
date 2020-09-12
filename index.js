94562a6ceeb700a05476b20ae6b77ca03d546665
nbm <notbigmuzzy@gmail.com>
2020-09-12 02:13:28 +0200

    Refactor CV page and pull it in the new setup

--- index.js
+++ index.js
@@ -44,4 +44,11 @@ $(document).ready(function () {
             }
         }
     }
+
+    //PREVENT CLICK ON CV TO CLOSE
+    $('#cv-toggle + label .the-cv').click(function(e) {
+        if ( $('#cv-toggle').is(":checked") ) {
+            e.preventDefault()
+        }
+    })
 })
