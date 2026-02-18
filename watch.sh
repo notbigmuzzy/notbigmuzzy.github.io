65b1a9c7de050abff89ea5ecad0686ce7047f202
Ivan Jovanovic <notbigmuzzy@gmail.com>
2026-02-18 12:31:35 +0100

    update local build system

--- watch.sh
+++ watch.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-echo "Starting Pug and Sass watchers..."
-
-title() { :; }
-
-pug -w assets/templates/index.pug -o ./ &
-pug -w assets/templates/c64.pug -o ./ &
-pug -w assets/templates/vapor.pug -o ./ &
-pug -w assets/templates/cv.pug -o ./ &
-
-sass --watch assets/styles/index.scss:index.css --style=compressed &
-
-echo "All watchers started in background. Use 'jobs' to see them."
+echo "Starting dev server..."
+npm run dev
